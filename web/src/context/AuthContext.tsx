73bf6e853907d3bab541a99e230cdd05e3fd3b07
Taras Pasternak <pasternak@MacBook-Pro.local>
2025-11-29 19:00:03 +0200

    Fix Vercel routing and update AuthContext

--- web/src/context/AuthContext.tsx
+++ web/src/context/AuthContext.tsx
@@ -60,6 +60,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
                     data: {
                         username: user.username,
                     },
+                    emailRedirectTo: window.location.origin,
                 },
             });
 
